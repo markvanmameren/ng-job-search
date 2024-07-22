960b4fa51e7148a23cdf8b6de0ed84e0885fa697
Mark van Mameren <mark.van.mameren@divotion.com>
2024-07-22 11:51:16 +0200

    design checks

--- src/app/components/favorite-jobs/favorite-jobs.component.ts
+++ src/app/components/favorite-jobs/favorite-jobs.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { JobService } from '../../services/job/job.service';
 import { JobCardsComponent } from '../job-cards/job-cards.component';
 
 @Component({
   selector: 'app-favorite-jobs',
   standalone: true,
-  imports: [JobCardsComponent],
+  imports: [JobCardsComponent, RouterLink],
   templateUrl: './favorite-jobs.component.html',
   styleUrl: './favorite-jobs.component.css',
 })
